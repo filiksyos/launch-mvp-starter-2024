964f9ed2cf74c2d6bce6e3b38d7d6c2add87ff74
Fili <126945152+filiksyos@users.noreply.github.com>
2025-10-08 16:54:34 +0300

    Add Next.js configuration

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'X-Frame-Options',
+          value: 'DENY',
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff',
+        },
+        {
+          key: 'Referrer-Policy',
+          value: 'strict-origin-when-cross-origin',
+        },
+      ],
+    },
+  ],
+};
+
+export default nextConfig;
